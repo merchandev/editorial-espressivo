348a75e0e4fde0b4ce31331b6562c6a8f9813b1a
merchandev <merchan.dev@hotmail.com>
2026-05-26 10:47:54 -0400

    fix: Resolve 404 search form visual bug by removing label wrapper

--- searchform.php
+++ searchform.php
@@ -4,10 +4,7 @@
  */
 ?>
 <form role="search" method="get" class="search-form custom-search-form" action="<?php echo esc_url( home_url( '/' ) ); ?>">
-    <label>
-        <span class="screen-reader-text"><?php echo _x( 'Buscar por:', 'label', 'pro' ); ?></span>
-        <input type="search" class="search-field" placeholder="<?php echo esc_attr_x( 'Buscar noticias...', 'placeholder', 'pro' ); ?>" value="<?php echo get_search_query(); ?>" name="s" autocomplete="off" />
-    </label>
+    <input type="search" class="search-field" placeholder="<?php echo esc_attr_x( 'Buscar noticias...', 'placeholder', 'pro' ); ?>" value="<?php echo get_search_query(); ?>" name="s" autocomplete="off" aria-label="<?php echo esc_attr_x( 'Buscar por:', 'label', 'pro' ); ?>" />
     <button type="submit" class="search-submit" aria-label="Buscar">
         <span class="material-symbols-outlined">search</span>
     </button>
